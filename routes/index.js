478f7c6f02a5efecb339092ae731b3757665585e
Kasumba <kraymond1738@gmail.com>
2023-09-20 06:44:12 +0000

    new Routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const UserController = require('../controllers/userController');
+//const movieController = require('../controllers/movieController');
 
 router.get('/signup(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'html', 'signup.html'));
@@ -11,10 +12,30 @@ router.get('/login(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'html', 'login.html'));
 });
 
+router.get('/landing(.html)?', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'landing.html'));
+});
+
+router.get('/home(.html)?', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'Home.html'));
+});
+
+router.get('/About(.html)?', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'About.html'));
+});
+
+router.get('/Blog(.html)?', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'Blog.html'));
+});
+
+router.get('/contact(.html)?', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'public', 'html', 'contact.html'));
+});
+
 router.post('/auth/signup', UserController.signup);
 
 router.post('/auth/login', UserController.login);
 
-router.post('/movie/search', movieController.movieSearch);
+//router.post('/movie/search', movieController.movieSearch);
 
 module.exports = router;
